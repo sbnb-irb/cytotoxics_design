139aa5ee8f8a910094d7dacf16ca22fea15e2cb0
GemaRG96 <gema.rojas@irbbarcelona.org>
2025-10-03 15:29:22 +0200

    new arrangement of files and update files to final paper version

---
+++ cytotoxicity prediction models/example_usage_run_aloy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+singularity exec --cleanenv --contain --nv --bind $PWD:/repo --bind /aloy/scratch/:/aloy/scratch reinvent3_signaturizer_kbest.simg python /repo/run_predictions.py \
+    --model /repo/models/BXPC3.p \
+    --kbest_reduction /repo/kbest_dim_reduction/BXPC3.p \
+    --smiles_file /repo/example_smiles.txt \
+    --output_file /repo/BXPC3_predictions.csv
